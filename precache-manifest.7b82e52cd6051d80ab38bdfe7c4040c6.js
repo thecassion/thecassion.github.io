bc4fcd59b67cba9e309d6f27a01dfeae901b9ed8
thecassion <pierrerobentz.cassion@gmail.com>
2019-04-18 15:45:13 -0400

    Updates

---
+++ precache-manifest.7b82e52cd6051d80ab38bdfe7c4040c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83f871ab394e5828c4bd",
+    "url": "/static/css/main.2b337a87.chunk.css"
+  },
+  {
+    "revision": "83f871ab394e5828c4bd",
+    "url": "/static/js/main.33ff1f55.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "dab46a7662fb38cb8b9175e10a5049d7",
+    "url": "/static/media/background1.dab46a76.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dcf64c0b0392656f00e6b09a5583e22e",
+    "url": "/index.html"
+  }
+];
